b49928ee7a9dd11faf8baa9e56936d99d21f622a
Ali ghahremani <alighahremani1377@gmail.com>
2024-12-03 13:52:15 +0330

    new: day-01 part2

--- 2024/day-01/src/main.rs
+++ 2024/day-01/src/main.rs
@@ -1,7 +1,7 @@
-use std::fs;
+use std::{collections::HashMap, fs};
 
 fn main() {
-    part_1();
+    part_2();
 }
 
 fn part_1() {
@@ -26,4 +26,35 @@ fn part_1() {
         sum_diff += diff;
     }
     println!("sum_diff: {}", sum_diff);
+}
+
+fn part_2() {
+    let lines = fs::read_to_string("./input.txt").unwrap();
+
+    let mut list1 = Vec::<u32>::new();
+    let mut list2 = Vec::<u32>::new();
+    for line in lines.lines() {
+        let sp: Vec<&str> = line.split("   ").collect();
+        list1.push(sp[0].parse().unwrap());
+        list2.push(sp[1].parse().unwrap());
+    }
+
+    let mut map = HashMap::<u32, u32>::new();
+
+    for i in list2 {
+        match map.get_mut(&i) {
+            Some(x) => {
+                *x += 1;
+            }
+            None => {
+                map.insert(i, 1);
+            }
+        }
+    }
+
+    let mut sum: u32 = 0;
+    for i in list1 {
+        sum += i * map.get(&i).unwrap_or(&0);
+    }
+    println!("sum: {}", sum)
 }
